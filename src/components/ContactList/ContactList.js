22a7a588f18c149605aafee6e59570c5c8f61b15
Anastasiia Yermakova <yermak1037@gmail.com>
2021-01-14 20:58:37 +0200

    delete dnd and add transtion animation

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,80 +1,35 @@
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  deleteContact,
-  dndContact,
-} from '../../redux/contacts/contacts-actions';
-import { getContacts } from '../../redux/contacts/contacts-selectors';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import { deleteContact } from '../../redux/contacts/contacts-actions';
+import { getVisibleContacts } from '../../redux/contacts/contacts-selectors';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { ReactComponent as DeleteIcon } from '../../img/delete.svg';
 import s from './ContactList.module.css';
-
-// a little function to help us with reordering the result
-const reorder = (list, startIndex, endIndex) => {
-  const result = [...list];
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-
-  return result;
-};
+import popTransition from './transitions/pop.module.css';
 
 function ContactList() {
   const dispatch = useDispatch();
-  const contacts = useSelector(getContacts);
-
-  const onDragEnd = result => {
-    const { source, destination } = result;
-
-    if (!destination) return;
-
-    if (
-      destination.index === source.index &&
-      destination.droppableId === source.droppableId
-    )
-      return;
-
-    const items = reorder(contacts, source.index, destination.index);
-
-    dispatch(dndContact(items));
-  };
+  const contacts = useSelector(getVisibleContacts);
 
   return contacts.length > 0 ? (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="contacts">
-        {provided => (
-          <ul
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-            className={s.list}
-          >
-            {contacts.map(({ id, name, number }, index) => (
-              <Draggable key={id} draggableId={id} index={index}>
-                {provided => (
-                  <li
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    className={s.item}
-                  >
-                    <p className={s.info}>
-                      <b>{name}</b>
-                      <em>{number}</em>
-                    </p>
-                    <button
-                      className={s.btn}
-                      type="button"
-                      onClick={() => dispatch(deleteContact(id))}
-                    >
-                      <DeleteIcon width="26" height="26" />
-                    </button>
-                  </li>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </ul>
-        )}
-      </Droppable>
-    </DragDropContext>
+    <TransitionGroup component="ul" className={s.list}>
+      {contacts.map(({ id, name, number }) => (
+        <CSSTransition key={id} timeout={500} classNames={popTransition}>
+          <li className={s.item}>
+            <p className={s.info}>
+              <b>{name}</b>
+              <em>{number}</em>
+            </p>
+            <button
+              className={s.btn}
+              type="button"
+              onClick={() => dispatch(deleteContact(id))}
+            >
+              <DeleteIcon width="26" height="26" />
+            </button>
+          </li>
+        </CSSTransition>
+      ))}
+    </TransitionGroup>
   ) : (
     <p>Your phonebook is empty. Please add contact.</p>
   );
--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,11 +1,6 @@
 import shortid from 'shortid';
 import { createAction } from '@reduxjs/toolkit';
-import {
-  ADD_CONTACT,
-  DELETE_CONTACT,
-  DND_CONTACT,
-  FILTER_CONTACT,
-} from './contacts-types';
+import { ADD_CONTACT, DELETE_CONTACT, FILTER_CONTACT } from './contacts-types';
 
 export const addContact = createAction(ADD_CONTACT, (name, number) => ({
   payload: {
@@ -15,5 +10,4 @@ export const addContact = createAction(ADD_CONTACT, (name, number) => ({
   },
 }));
 export const deleteContact = createAction(DELETE_CONTACT);
-export const dndContact = createAction(DND_CONTACT);
 export const filterContact = createAction(FILTER_CONTACT);
